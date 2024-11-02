e9bc067be4f9f57b4bdd4d361eb6b79386f47ea8
ITboo <annaacdc@tut.by>
2024-11-02 19:48:32 +0300

    chore: add artist endpoint

--- src/common/messages.ts
+++ src/common/messages.ts
@@ -1,7 +1,9 @@
 export enum ERROR {
   ID = 'ERROR: id must be UUID',
   USER_NOT_FOUND = 'ERROR: User not found',
+  ARTIST_NOT_FOUND = 'ERROR: Artist not found',
   LOGIN_PASSWORD_REQUIRED = 'Login and password are required',
   BOTH_PASSWORDS_REQUIRED = 'New password and old password are required',
-  INCORRECT_OLD_PASSWORD = 'Old password is incorrect'
+  INCORRECT_OLD_PASSWORD = 'Old password is incorrect',
+  INVALID_ARTIST_REQUEST = 'Name and grammy are required'
 }
--- src/database/data.ts
+++ src/database/data.ts
@@ -1,8 +1,11 @@
+import { Artist } from "src/modules/artist/entities/artist.entity";
 import { User } from "src/modules/user/entities/user.entity";
 
 interface Database {
   users: User[];
+  artists: Artist[];
 }
 export const db: Database = {
   users: [],
+  artists: [],
 };
